10aeeea32b52a2e51ce7c6378f01ff1f991bcc6a
Artem Haharin <artem.haharin@sigma.software>
2022-12-02 14:18:46 +0200

    Run dotnet format

--- OnlineStore.API/Controllers/WeatherForecastController.cs
+++ OnlineStore.API/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace OnlineStore.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -25,7 +25,7 @@ namespace OnlineStore.API.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
             })
             .ToArray();
         }
